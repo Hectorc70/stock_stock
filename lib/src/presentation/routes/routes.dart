63297e8f5ddf6d937061fe0488dd123b83839f49
Hector-Home <hecgarciann@gmail.com>
2021-12-04 23:53:26 -0600

    register page

--- lib/src/presentation/routes/routes.dart
+++ lib/src/presentation/routes/routes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:stock_stock/src/presentation/pages/login_page/login_page.dart';
+import 'package:stock_stock/src/presentation/pages/register_page/register_page.dart';
 import 'package:stock_stock/src/presentation/pages/splash_page/splash_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(BuildContext context) {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => SplashPage.init(context),
     'loginPage': (BuildContext context) => const LoginPage(),
+    'registerPage': (BuildContext context) => const RegisterPage(),
     'HomePage': (BuildContext context) => const LoginPage(),
   };
 }
